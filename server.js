aaea402e604b603d0db3e668031eab9ec1cdc058
Bas3L3ss <phamthen.hun060907@gmail.com>
2024-12-16 14:34:14 +0700

    Add video processing and AWS S3 integration
    - Implemented video upload to AWS S3 using the AWS SDK.
    - Added OpenAI integration for audio transcription and content generation.
    - Enhanced socket event handling for video processing, including error handling and file management.
    - Configured environment variables for sensitive credentials.

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const http = require("http");
 const app = express();
 const dotenv = require("dotenv");
 const { Readable } = require("stream");
+const axios = require("axios");
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const OpenAi = require("openai");
 
 dotenv.config();
 
@@ -18,6 +21,18 @@ const io = new Server(server, {
     origin: process.env.NEXT_ELECTRON_HOST,
     methods: ["GET", "POST"],
   },
+});
+
+const s3 = new S3Client({
+  credentials: {
+    accessKeyId: process.env.ACCESS_KEY,
+    secretAccessKey: process.env.SECRET_KEY,
+  },
+  region: process.env.BUCKET_REGION,
+});
+
+const openai = new OpenAi({
+  apiKey: process.env.OPEN_AI_KEY,
 });
 
 let recordedChunks = [];
@@ -42,6 +57,95 @@ io.on("connection", (socket) => {
   });
   socket.on("process-video", async (data) => {
     console.log("Processing video...", data);
+    recordedChunks = [];
+    fs.readFile("temp_upload/" + data.filename, async (err, file) => {
+      const processing = await axios.post(
+        `${process.env.NEXT_API_HOST}recording/${data.userId}/processing`,
+        {
+          filename: data.filename,
+        }
+      );
+      if (processing.data.status !== 200)
+        return console.log(
+          "ERROR SOMETHING WENT WRONG WITH CREATING THE PROCESSING FILE"
+        );
+      const Key = data.filename;
+      const Bucket = process.env.BUCKET_NAME;
+      const ContentType = "video/webm";
+      const command = new PutObjectCommand({
+        Key,
+        Bucket,
+        ContentType,
+        Body: file,
+      });
+
+      const fileStatus = await s3.send(command);
+
+      if (fileStatus["$metadata"].httpStatusCode == 200) {
+        console.log("Video updated to AWS ");
+
+        if (processing.data.plan === "PRO") {
+          fs.stat(`temp_upload/${data.filename}`, async (err, stat) => {
+            if (!err) {
+              // whisper 25mb
+              if (stat.size < 25000000) {
+                const transcription = await openai.audio.transcriptions.create({
+                  file: fs.createReadStream(`temp_upload/${data.filename}`),
+                  model: "whisper-1",
+                  response_format: "text",
+                });
+
+                if (transcription) {
+                  const completion = await openai.chat.completions.create({
+                    model: "gpt-3.5-turbo",
+                    response_format: { type: "json_object" },
+                    messages: [
+                      {
+                        role: "system",
+                        content: `You are going to generate a title and a nice description using the speech to text transcription (${transcription}) and then return it in json format as {"title":<the title you gave>,"summary":<the summary you created> }`,
+                      },
+                    ],
+                  });
+
+                  const titleAndSummeryGenerated = await axios.post(
+                    `${process.env.NEXT_API_HOST}recording/${data.userId}/transcribe`,
+                    {
+                      filename: data.filename,
+                      content: completion.choices[0].message.content,
+                      transcript: transcription,
+                    }
+                  );
+
+                  if (titleAndSummeryGenerated.data.status !== 200) {
+                    console.log(
+                      "ERROR SOMETHING WENT WRONG WITH CREATING THE PROCESSING FILE"
+                    );
+                  }
+                }
+              }
+            }
+          });
+        }
+        const stopProcessing = await axios.post(
+          `${process.env.NEXT_API_HOST}recording/${data.userId}/complete`,
+          {
+            filename: data.filename,
+          }
+        );
+        if (stopProcessing.status !== 200) {
+          console.log("SOMETHING WENT WRONG DURING STOP PROCESSING");
+        }
+        if (stopProcessing.status === 200) {
+          fs.unlink(`temp_upload/${data.filename}`, (err) => {
+            if (!err) {
+              console.log(`${data.filename} deleted successfully`);
+            }
+          });
+        }
+      } else {
+        console.log("ERROR, UPLOAD FAILED! PROCESS ABORTED");
+      }
+    });
   });
   socket.on("disconnect", async (data) => {
     console.log("Socket.id is disconnected ", socket.id);
